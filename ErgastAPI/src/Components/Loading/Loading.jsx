9b67bcecad707cae432d55d9fa477186d1caee84
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-04-01 21:26:26 +0200

    Restructure the architecture

---
+++ ErgastAPI/src/Components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div aria-label="Loading..." className="loading"></div>
+  )
+}
+
+export default Loading
---
+++ ErgastAPI/src/Components/Table/Table.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Position</th>
+          <th>Driver</th>
+          <th>Points</th>
+          <th>Wins</th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+export default Table
